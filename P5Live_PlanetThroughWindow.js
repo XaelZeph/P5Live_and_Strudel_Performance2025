60ff2767a97a57f1a417441ffffc1d6f5ed301f6
XaelZeph <xaelzephyr05@gmail.com>
2025-10-22 12:45:39 -0400

    Added better documentation to p5live code
    Added in more comments for p5live code which explains how the code runs better. Cleaned up code so that test code is erased

--- P5Live_PlanetThroughWindow.js
+++ P5Live_PlanetThroughWindow.js
@@ -19,7 +19,7 @@ for(let i = 1; i < synthCount; i++) {
 	hc[i].width = 200 // window.innerWidth // for full res
 	hc[i].height = 200 // window.innerHeight // for full res
 	
-	document.body.appendChild(hc[i]);
+	// document.body.appendChild(hc[i]);
 
 	synth[i] = new Hydra({
 		// detectAudio: false, // no mic
@@ -30,32 +30,41 @@ for(let i = 1; i < synthCount; i++) {
 	}).synth // scoped hydra
 }
 
-	hc[0] = document.createElement('canvas') // hydra canvas + custom size
-	hc[0].width =  window.innerWidth // for full res
-	hc[0].height = window.innerHeight // for full res
-	
-	document.body.appendChild(hc[0]);
+// hc[0] is created separately as it has detectAudio:true
+// and the resolution is lower to lower lag
+// I believe it will just choose which one to apply based on which hydra has
+// src(s0).layer(o0).out(o0)
+hc[0] = document.createElement('canvas') // hydra canvas + custom size
+hc[0].width =  window.innerWidth // for full res
+hc[0].height = window.innerHeight // for full res
 
-	synth[0] = new Hydra({
-		// detectAudio: false, // no mic
-		// autoLoop: true,
-		detectAudio: true,
-		canvas: hc[0], // use hydra canvas
-		makeGlobal: false, // scoped
-	}).synth // scoped hydra
+document.body.appendChild(hc[0]);
 
-// synth[0] = {detectAudio: true}
+synth[0] = new Hydra({
+	// detectAudio: false, // no mic
+	// autoLoop: true,
+	detectAudio: true,
+	canvas: hc[0], // use hydra canvas
+	makeGlobal: false, // scoped
+}).synth // scoped hydra
 
 
 // sandbox - start
 // access each instance via synth[index]
+
+// set up audio for hydra 0
 synth[0].a.show() // a.hide() to remove
 synth[0].a.setBins(3)
 synth[0].a.setSmooth(.8)
 
-synth[0].src(synth[0].o0).layer(synth[0].src(synth[0].s0)
+// overall texture (applied to the entire screen)
+// each code section has a "header" which describes what each modulation does
+// found that synth[#] does NOT like verticle spaces, it detects "end" of one hydra texture
+// with verticle spaces
+synth[0].src(synth[0].o0)
+	 .layer(synth[0].src(synth[0].s0)
      .modulate(synth[0].osc(100), .01)
-      .modulateScrollY(synth[0].osc(5), ()=>sin(frameCount/50*3)*.05)
+     .modulateScrollY(synth[0].osc(5), ()=>sin(frameCount/50*3)*.05)
      )
      .scale(0.8).modulate(synth[0].osc(100), .01).modulate(synth[0].osc(8,-0.5, 1).blend(synth[0].o0))
 	 .modulate(synth[0].shape(4).rotate(0.5, 0.5).scale(2)
@@ -80,96 +89,84 @@ synth[0].src(synth[0].o0).layer(synth[0].src(synth[0].s0)
 	 // .modulateScale(synth[2].src(synth[2].o0).scale(1.01), ()=>(synth[0].a.fft[0]))
 .out(synth[0].o0)
 	
-// ball
+// ball texture
 synth[1].noise()
 .brightness(0.25)
 // changes color based on audio (which is initialized by only synth[0])
 .color(0.5, 1, 0.7).hue(()=>synth[0].a.fft[0])
 .out()
 
-// flames
+// flames texture
 synth[2]
 // changes color based on audio (which is initialized by only synth[0])
 .solid(0.5, 1, 0.7).hue(()=>synth[0].a.fft[0])
 .out()
-
 // sandbox - stop
 
 function setup() {
-	canvas1 = createCanvas(windowWidth, windowHeight, WEBGL)
-	canvas1.hide()
-	canvas2 = createCanvas(windowWidth, windowHeight, WEBGL)
+	createCanvas(windowWidth, windowHeight, WEBGL)
 	background(0)
 	noStroke()
 	
+	// starts up particle systems (ps is the big ball flames, ps2 is the background tiny flickering flames)
 	ps = new ParticleSystem(createVector(width / 2, 50, 50));
 	ps2 = new ParticleSystem2(createVector(width / 2, 50, 50));
 	
+	// this starts up the whole screen texture
 	synth[0].s0.init({
 		src: canvas
 	})
 	
-	pg[0] = createGraphics(hc[0].width, hc[0].height)
-	// bg = createGraphics(width, height, WEBGL)
 	// prep synth layers
-	for(let i = 1; i < synthCount; i++) {
+	for(let i = 0; i < synthCount; i++) {
 		pg[i] = createGraphics(hc[i].width, hc[i].height)
-	// 	pg[i].clear()
-	// pg[i].noFill()
-	// pg[i].stroke(255)
-	// pg[i].strokeWeight(3)
 		pg[i].drawingContext.drawImage(hc[i], 0, 0, pg[i].width, pg[i].height)
 	}	
-	
-	
 }
 
 function draw() {
 	// orbitControl(5)
 	
+	// note that and background(0) are necessary to reset the board
+	// so that the entire screen doesn't get filled with modulations
+	// try getting rid of one or the other, see the effect it creates!
 	clear()
 	background(0)
 	// rotateX(frameCount / 15)
 	// rotateY(frameCount / 5)
 	
 
-	// grab + apply hydra textures
-	for(let i = 1; i < synthCount; i++) {
+	// grab + apply hydra textures 
+	for(let i = 0; i < synthCount; i++) {
 		pg[i].clear()
 		pg[i].drawingContext.drawImage(hc[i], 0, 0, pg[i].width, pg[i].height)
 	}
 	
-	// rotateY(radians(90) * frameCount/50)
-	// texture(pg[1])
-	// torus(height/5, 40)
-	
-	// push()
-	// // texture(pg[0]) // use hydra 1
-	// rotateX(radians(frameCount/50 / 8))
-	// rotateZ(radians(-frameCount/50 / 6))
-	// torus(height/5, 40)
-	// pop()
-	
+	// One way to use audio within other draw() code (not just in Hydra section)
 	let cFlame = color(synth[0].a.fft[0] * height / 2, synth[0].a.fft[1] * height / 2, synth[0].a.fft[2] * height / 2)
 	// let cFlame = color(synth[0].a.fft[0] * height / 2, synth[0].a.fft[0] * height / 2, synth[0].a.fft[0] * height / 2)
-	//let cFlame = color(255)
-	// push()
-	// fill(cFlame)
-	// buildFlame();
-	// pop()
+	// let cFlame = color(255)
 	
+	// creates a flame which follows the mouse:
+	push()
+	fill(cFlame)
+	buildFlame();
+	pop()
+	
+	// creates moving flames 
 	for (let i = 1; i < 4; i ++) {
 		push()
 		x1 = noise(frameCount * 0.002 * i) * width - (300 * i)
 		y1 = noise(frameCount * 0.003 * i) * height - (150*i)
 		s1 = noise(frameCount * 0.02 * i) * 30 + 10
 		// fill(cFlame)
-		texture(pg[2]) // use hydra for sphere
+		texture(pg[2]) // use hydra for sphere flames
 		buildFlame(x1, y1, s1);
 		pop()
 		
 	}
 	
+	// create background flickering flames
 	push()
 	// fill(cFlame)
 	texture(pg[2])
@@ -177,20 +174,16 @@ function draw() {
 		buildFlameStatic(random(-700, 500), random(-300, 300), random(-500, 0))
 	}
 	pop()
-
-	texture(pg[1]) // use hydra for ball
-	// translate(0)
+	
+	// middle ball 
 	push()
-	rotateY(frameCount/50/3)
-	rotateX(frameCount/50/4)
+	texture(pg[1]) // use hydra for ball
+	rotateY(frameCount/3)
+	rotateX(frameCount/3)
 	sphere(height/5, 40)
 	pop()
 	
-	// texture(pg[2]) // use hydra 0
-	// translate(0, height/4)
-	// rotateY(radians(90))
-	// torus(height/5, 40)
-	
+	// middle box
 	push()
 	fill(0)
 	stroke(255)
@@ -200,17 +193,19 @@ function draw() {
 	pop()
 	
 
-	
+	// right corner boxes
 	push()
 	fill(0)
 	stroke(255)
 	strokeWeight(2)
-	translate(600, 255, 0)
+	translate(550, 230, 0)
 	rotateX(frameCount/50)
 	rotateY(frameCount/50)
 	box(100)
 	pop()
 	
+
+	// left corner boxes
 	push()
 	fill(0)
 	stroke(255)
@@ -232,8 +227,8 @@ function draw() {
 	
 }
 
+// first particle system, creates the larger sphere flames
 class Particle {
-
   constructor(x, y, z) {
     this.acceleration = createVector(0, -0.1, 0);
     this.velocity = createVector(random(-0.3, 0.3), random(0, 0.1), random(-0.3, 0.3));
@@ -282,6 +277,7 @@ class Particle {
   }
 }
 
+
 class ParticleSystem {
 
   constructor(x, y, z) {
@@ -309,31 +305,43 @@ class ParticleSystem {
   }
 }
 
+// builds sphere flame, if x y are undefined, 
 function buildFlame(x, y, s) {
 	// glowing sphere
 	  // emissiveMaterial(255, 0, 0, 0.2)
 	  if (x !== undefined && y !== undefined && s !==undefined) {
 	  	push();
-	  translate(x, y, 65);
-	  sphere(s)
-	  pop();
-	  ps.run();
-	  push();
-	  ps.origin.set(x, y, 65);
-	  ps.addParticle();
-	  pop();
+		translate(x, y, 65);
+		sphere(s)
+		pop();
+		ps.run();
+		push();
+		ps.origin.set(x, y, 65);
+		ps.addParticle();
+		pop();
+	  }
+	  else if (s !== undefined) {
+		push();
+		translate(mouseX - windowWidth/2, mouseY - windowHeight/2, 65);
+		sphere(s)
+		pop();
+		ps.run();
+		push();
+		ps.origin.set(mouseX - windowWidth/2, mouseY - windowHeight/2, 65);
+		ps.addParticle();
+		pop();
 	  }
 	  else {
-	  	push();
-	  translate(mouseX - windowWidth/2, mouseY - windowHeight/2, 65);
-	  sphere(sphereSize)
-	  pop();
-	  ps.run();
-	  push();
-	  ps.origin.set(mouseX - windowWidth/2, mouseY - windowHeight/2, 65);
-	  ps.addParticle();
-	  pop();
-		}
+		push();
+		translate(mouseX - windowWidth/2, mouseY - windowHeight/2, 65);
+		sphere(sphereSize)
+		pop();
+		ps.run();
+		push();
+		ps.origin.set(mouseX - windowWidth/2, mouseY - windowHeight/2, 65);
+		ps.addParticle();
+		pop();
+	}
 }
 
 class Particle2 {
